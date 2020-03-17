d582df1d49ac7573c74d9533e6772692fbcbb018
nomsa <nomsam@azuka.co.za>
2020-03-17 09:54:46 +0200

    added user feeds/tweets

--- resources/views/layouts/feeds.blade.php
+++ resources/views/layouts/feeds.blade.php
@@ -14,14 +14,14 @@
             @else
                 @foreach($users as $user)
 
-                    <div class=" bg-light text-dark flex-fill p-2"><u><a href="{{URL::to('/user_name/'.$user->user_name)}}" class="">{{ $user->user_name }}</a></u>
+                    <div class=" bg-light text-dark flex-fill p-2"><u><a href="{{URL::to('/user/'.$user->user_name)}}" class="">{{ $user->user_name }}</a></u>
                         <div class="border border-primary"></div><br/>
 
                                 @if(!is_null($user->user_ids))
                                 <label class="text-dark font-italic">follows</label>
                                     @foreach(explode('|',$user->user_ids) as $follows)
                                         @foreach($user->where('id', $follows)->pluck('user_name') as $user_name)
-                                            <a href="{{URL::to('/user_name/'.$user_name)}}" class=" btn-info text-light p-2 m-2">{{$user_name}}</a>
+                                            <a href="{{URL::to('/user/'.$user_name)}}" class=" btn-info text-light p-2 m-2">{{$user_name}}</a>
                                         @endforeach
                                     @endforeach
 
@@ -41,7 +41,6 @@
         <div class="card-header">{{ $title }}</div>
 
     </div>
-
         @if(isset($feeds) && count($feeds) == 0)
             <div class="card-body bg-warning">
             <div class=" ">No Feeds in this timeline</div>
@@ -60,9 +59,5 @@
                 @endforeach
             </div>
         @endif
-
-    <div class="card-footer">
-        <div class="btn btn-danger">Back</div>
-    </div>
 
     @endsection
